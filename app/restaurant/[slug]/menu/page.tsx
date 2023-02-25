1f1700faa8b4c49c0966b817c1fbc2ec79505338
Igor <w0rz@protonmail.com>
2023-02-25 18:29:43 -0300

    NavBar component moved to root layout & wrapped with <></>

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,22 +1,17 @@
-import NavBar from "@/app/components/NavBar"
-import Header from "../components/Header"
-import Menu from "../components/Menu"
-import RestaurantNavBar from "../components/RestaurantNavBar"
+import Header from "../components/Header";
+import Menu from "../components/Menu";
+import RestaurantNavBar from "../components/RestaurantNavBar";
 
 export default function RestaurantMenuPage() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-<Header />
+  <>
+        <Header />
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[100%] p-3 shadow rounded">
             <RestaurantNavBar />
-<Menu />
-</div>
+            <Menu />
+          </div>
         </div>
-        {/* Description end */}
-      </main>
-    </main>
-  )
+  </>
+  );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,34 +1,29 @@
-import NavBar from "@/app/components/NavBar"
-import Description from "./components/Description"
-import Gallery from "./components/Gallery"
-import Header from "./components/Header"
-import Rating from "./components/Rating"
-import ReservationCard from "./components/ReservationCard"
-import RestaurantNavBar from "./components/RestaurantNavBar"
-import Reviews from "./components/Reviews"
-import Title from "./components/Title"
+import Description from "./components/Description";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+import Rating from "./components/Rating";
+import ReservationCard from "./components/ReservationCard";
+import RestaurantNavBar from "./components/RestaurantNavBar";
+import Reviews from "./components/Reviews";
+import Title from "./components/Title";
 
-export default function RestaurantDetails(){
+export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-      <NavBar /> 
-       <Header />
+<>
+<Header />
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[70%] p-3 shadow rounded">
-<RestaurantNavBar />
+            <RestaurantNavBar />
             <Title />
             <Rating />
-           <Description />
-         <Gallery />
-         <Reviews />
-         </div>
-        <div className="w-[27%] relative text-reg">
-        <ReservationCard />
+            <Description />
+            <Gallery />
+            <Reviews />
+          </div>
+          <div className="w-[27%] relative text-reg">
+            <ReservationCard />
+          </div>
         </div>
-        </div>
-      </main>
-    </main>
-  )
+ </>
+ );
 }
-
