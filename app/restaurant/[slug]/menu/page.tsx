8e4f1def62195babde002850da4ff3e240779d2f
Igor <w0rz@protonmail.com>
2023-02-25 18:54:51 -0300

    DRY principle applied to components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -5,13 +5,10 @@ import RestaurantNavBar from "../components/RestaurantNavBar";
 export default function RestaurantMenuPage() {
   return (
   <>
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] p-3 shadow rounded">
+ <div className="bg-white w-[100%] p-3 shadow rounded">
             <RestaurantNavBar />
             <Menu />
           </div>
-        </div>
   </>
   );
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import Description from "./components/Description";
 import Gallery from "./components/Gallery";
-import Header from "./components/Header";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
 import RestaurantNavBar from "./components/RestaurantNavBar";
@@ -10,8 +9,6 @@ import Title from "./components/Title";
 export default function RestaurantDetails() {
   return (
 <>
-<Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[70%] p-3 shadow rounded">
             <RestaurantNavBar />
             <Title />
@@ -23,7 +20,6 @@ export default function RestaurantDetails() {
           <div className="w-[27%] relative text-reg">
             <ReservationCard />
           </div>
-        </div>
  </>
  );
 }
