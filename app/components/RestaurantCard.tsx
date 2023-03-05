c02118e1fc718bec59e5d2b77899e7af56bbc42a
Igor <w0rz@protonmail.com>
2023-03-05 19:31:18 -0300

    Rendering the fecthed data

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,26 +1,31 @@
-import React from "react";
 import Link from "next/link";
+import { RestaurantCardType } from "../page";
+import Price from "./Price";
 
-export default function RestaurantCard() {
+interface Props {
+  restaurant: RestaurantCardType;
+}
+
+export default function RestaurantCard({ restaurant }: Props) {
   return (
-    <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-      <Link href="/restaurant/nobu-jardins-sao-paulo">
+    <div className="w-64 h-71 m-3 rounded overflow-hidden border cursor-pointer">
+      <Link href={`/restaurant/${restaurant.slug}`}>
         <img
-          src="https://resizer.otstatic.com/v2/photos/wide-huge/1/25924907.jpg"
+          src={restaurant.main_image}
           alt=""
           className="w-full h-36"
         />
         <div className="p-1">
-          <h3 className="font-bold text-2xl mb-2">Nobu Jardins São Paulo</h3>
+          <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">★★★★☆</div>
             <p className="ml-2">30 Reviews</p>
           </div>
         </div>
         <div className="flex text-reg font-light capitalize">
-          <p className="mr-3">Japanese</p>
-          <p className="mr-3">$$$$</p>
-          <p>Sao Paulo</p>
+          <p className="mr-3">{restaurant.cuisine.name}</p>
+          <Price price={restaurant.price} />
+          <p>{restaurant.location.name}</p>
         </div>
         <p className="text-sm mt-1 font-bold">Booked 0 times today</p>
       </Link>
