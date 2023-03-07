e0a9f67ffa1c2b353295e2239bcd72904e1c4cf9
Igor <w0rz@protonmail.com>
2023-03-07 18:11:57 -0300

    fetching restaurant by slug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import Description from "./components/Description";
 import Gallery from "./components/Gallery";
 import Rating from "./components/Rating";
@@ -6,20 +7,52 @@ import RestaurantNavBar from "./components/RestaurantNavBar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface RestarauntTypes {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<RestarauntTypes> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true
+    }
+  });
+  // Fix it later
+  if (!restaurant) {
+    throw new Error;
+  }
+  return restaurant;
+}
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log({ restaurant });
   return (
-<>
-          <div className="bg-white w-[70%] p-3 shadow rounded">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            <Gallery />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
- </>
- );
+    <>
+      <div className="bg-white w-[70%] p-3 shadow rounded">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Gallery />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
+  );
 }
