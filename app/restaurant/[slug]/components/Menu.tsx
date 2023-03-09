f9bd0142d7c9a414e4af2129d892e469c8fd7a06
Igor <w0rz@protonmail.com>
2023-03-09 02:14:31 -0300

    fetching restaurant's menu

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,14 +1,23 @@
+import { Items } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-export default function Menu() {
+export default function Menu({ menu }: { menu: Items[] }) {
   return (
     <main className="bg-white mt-5">
       <div>
         <h1 className="font-bold text-4xl">Menu</h1>
       </div>
-      <div className="flex flex-wrap justify-between">
-        <MenuCard />
-      </div>
+      {menu.length ? (
+        <div className="flex flex-wrap justify-between">
+          {menu.map(item => (
+            <MenuCard key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-wrap justify-between">
+          <p>This restaurant does not have a menu</p>
+        </div>
+      )}
     </main>
   );
 }
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,14 +1,32 @@
-import Header from "../components/Header";
+import { PrismaClient } from "@prisma/client";
 import Menu from "../components/Menu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 
-export default function RestaurantMenuPage() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      items: true
+    }
+  })
+  if (!restaurant) {
+    throw new Error
+  }
+  return restaurant.items
+}
+export default async function RestaurantMenuPage({ params }: { params: { slug: string } }) {
+  const items = await fetchRestaurantMenu(params.slug);
+
   return (
-  <>
- <div className="bg-white w-[100%] p-3 shadow rounded">
-            <RestaurantNavBar />
-            <Menu />
-          </div>
-  </>
+    <>
+      <div className="bg-white w-[100%] p-3 shadow rounded">
+        <RestaurantNavBar slug={params.slug} />
+        <Menu menu={items} />
+      </div>
+    </>
   );
 }
